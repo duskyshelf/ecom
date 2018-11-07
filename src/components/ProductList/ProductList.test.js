a5d49b4ac98901d453b7001a75c85376d4e2e5d3
DuskyShelf <dupsdale@gmail.com>
2018-11-07 20:21:21 +0000

    adds api call test

--- src/components/ProductList/ProductList.test.js
+++ src/components/ProductList/ProductList.test.js
@@ -1,5 +1,17 @@
+import React from 'react';
 import ProductList from './ProductList';
+import ProductCard from './ProductCard';
+import fetch from 'isomorphic-fetch';
+import { shallow } from 'enzyme';
 
-test('renders no products found when API returns an empty array', () => {
+jest.mock('isomorphic-fetch', () => () =>
+    Promise.resolve({ json: () => [{ title: 1 }] })
+);
 
-})
+test('renders 1 ProductCard when API returns a single item', async () => {
+    const productList = await shallow(<ProductList />);
+    await productList.instance().componentDidMount();
+    await productList.update();
+
+    expect(productList.find(ProductCard)).toHaveLength(1);
+});
